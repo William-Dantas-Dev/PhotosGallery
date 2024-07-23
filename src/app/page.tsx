e94cd177a87e73af13a3f96cbb949703c25996bf
william-dantas-dev <shireo.gaming.dev@gmail.com>
2024-07-23 16:43:03 -0300

    change title color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   return (
     <div className="min-h-screen bg-gray-800">
       <main className="flex flex-col items-center justify-center py-8">
-        <h1 className="text-4xl font-bold mb-8">Photos Gallery</h1>
+        <h1 className="text-4xl font-bold mb-8 text-white">Photos Gallery</h1>
         <PhotoGallery photos={photos} />
       </main>
     </div>
